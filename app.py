d0cbae0110c0f318895bdb807141647cca08c3fe
X2.Passion <essompassionshine@gmail.com>
2025-12-24 22:15:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if not st.session_state["logged_in"]:
             
             st.markdown("""
                 <div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>
-                    <p style='color: #7f8c8d; font-size: 15px;'>서울교통공사 뉴스 스크랩을 위한 공간입니다.<br>접속을 위해 비밀번호를 입력해주세요.</p>
+                    <p style='color: #7f8c8d; font-size: 15px;'>서울교통공사 뉴스 스크랩을 위한 공간입니다.<br>접속을 위해 비밀번호를 입력해주세요.<br> 시스템은 pc, 모바일 모두 사용 가능하지만 PC 사용을 권장합니다.</p>
                 </div>
                 """, unsafe_allow_html=True)
 
@@ -773,4 +773,5 @@ if st.session_state.search_results:
     if p_news: display_list("📰 지면 보도", p_news, "p")
     if n_news: display_list("🟢 네이버 뉴스", n_news, "n")
     if o_news: display_list("🌐 언론사 자체 뉴스", o_news, "o")
+
 
